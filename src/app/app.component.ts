c05ca5e3867bf0a30a0c19e604e770a212632c17
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-24 21:19:14 +0530

    [Ashutosh] Added Validate Username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,24 @@ export class AppComponent {
   imgUrl: string = 'assets/Bridgelabz.jpeg';
   url: string = 'https://www.bridgelabz.com';
   userName: string = '';
+  errorMessage: string = '';
 
   onClick(event: Event): void {
     console.log("BridgeLabz logo clicked!", event);
     window.open(this.url, "_blank");
+  }
+
+  validateUserName(): void {
+    const namePattern = /^[A-Z][a-zA-Z]{2,}$/;
+    if (!namePattern.test(this.userName)) {
+      this.errorMessage = 'Invalid Name! It should start with an uppercase letter and have at least 3 characters.';
+    } else {
+      this.errorMessage = ''; // Clear error when valid
+    }
+  }
+
+  resetUserName(): void {
+    this.userName = '';
+    this.errorMessage = '';
   }
 }
