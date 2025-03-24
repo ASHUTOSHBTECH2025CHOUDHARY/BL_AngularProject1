5d7e96325593446ba687f550d6ee20c4b28a9079
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-24 21:08:47 +0530

    [Ashutosh] Added Vist BridgeLABX WEBSITE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Hello from BridgeLabz';
-  message:string = 'Hello from BridgeLabz';
+  title: string = 'Hello from BridgeLabz'; 
+  message: string = 'Click the BridgeLabz Logo to visit the website!';
   imgUrl: string = 'assets/Bridgelabz.jpeg';
+  openBridgeLabz(): void {
+    window.open('https://www.bridgelabz.com/', '_blank');
+  }
 }
